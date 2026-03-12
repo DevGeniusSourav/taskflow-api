eb58a2a982780c0a373da31d4cf1bf1396c9e0eb
DevGeniusSourav <badasssourav5@example.com>
2026-03-13 01:55:42 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/sourav/taskflow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourav.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
